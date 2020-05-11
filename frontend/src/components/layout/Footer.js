7361f189e3f02c1ce39837c53113b5d52f53da52
Shikha Gupta <shiknema@gmail.com>
2020-05-11 23:19:18 +0200

    change in frontpage

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
+
+function Footer() {
+    return (
+        <div>
+             <Container id = "margin1">
+
+                    <Row>
+                        <Col>
+                        <p>covid-info<br />1177</p>
+                        </Col>
+                        <Col>
+                        <p>Emergency Number<br />112</p>
+                        </Col>
+                        <Col>
+                        <p>Medical Advice Number<br />11414</p>
+                        </Col>
+               </Row>
+       
+        </Container>
+        </div>
+    );
+}
+
+export default Footer;
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,14 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "./Logo.jpg"; 
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
 
-function Navbar({onLogout}) {
+
+
+function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">WomenIT</a>
+       <div>
+           
+           <Container id = "margin">
+              <Row >
+                        < Col>
+                            <img src={logo}  className ="header-img" />
+                        </Col>   
+                        <Col id = "colsetting">
+                            <h1>Positive Vibes</h1>
+                        </Col>    
+                        <Col id = "headerLink">
+                         <Link  style={{to:"#"}}>All Info about Covid-19</Link>
+                        </Col>      
+              </Row> 
+            </Container>
+       
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Positive Vibes</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-
             <div className="collapse navbar-collapse" id="navbarColor01">
                 <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
@@ -18,21 +37,24 @@ function Navbar({onLogout}) {
                         </li>
 
                         <li className="nav-item">
-                            <Link to="/posts" className="nav-link">
-                                Posts
+                            <Link to="/otherInformation" className="nav-link">
+                               Other Information
                             </Link>
                         </li>
 
                         <li className="nav-item">
-                            <Link to="/chat" className="nav-link">
-                                Chat
+                            <Link to="/contactUs" className="nav-link">
+                                Contact Us
                             </Link>
                         </li>
                 </ul>
-
-                <button className="btn btn-outline-info my-2 my-sm-0" onClick={onLogout}>Logout</button>
+               
             </div>
         </nav>
+       
+       
+
+</div>
     );
 }
 
