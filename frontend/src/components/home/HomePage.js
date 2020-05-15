cbe8371ffb3caf1d4298c3fc31e8ceca61188b65
Shikha Gupta <shiknema@gmail.com>
2020-05-15 14:38:01 +0200

    added AboutUs, SideBar files and did some changes in App.css

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -4,12 +4,32 @@ import SideBar from "../layout/SideBar.js";
 class HomePage extends Component {
     render() {
         return (
-           
-
             <div>
                  <div className="row">
                     <div className="col-sm-3">
-                    <SideBar/>
+                        <Container >
+                          <Row >
+                               <Alert variant="success" className ="alertCol">
+                                <Col ><Link to="/under3years">   Age Under 3 Years  </Link></Col>
+                                </Alert>
+                                
+                                <Alert variant="success"  className="alertCol">   
+                                <Col ><Link to="/age3to6Years">   Age 3 To 6 Years   </Link></Col>
+                                </Alert>
+
+                                <Alert variant="success" className="alertCol">   
+                                <Col><Link to="/age6to15Years">    Age 6 To 15 Years    </Link></Col>
+                                    </Alert>
+
+                                <Alert variant="success" className="alertCol">   
+                                <Col ><Link to="/age15to21Years">   Age 15 To 21 Years   </Link></Col>
+                                     </Alert>
+                                    
+                                <Alert variant="success" className="alertCol">   
+                                <Col ><Link to="/InternationalStudents"> International Students</Link></Col>
+                                </Alert>
+                            </Row>
+                        </Container>
                     </div>
                         <div className="col-sm-9" id ="Covid_Info" >
                             <h2> Covid -Information</h2> 
@@ -38,9 +58,7 @@ class HomePage extends Component {
                  </div>
                  
              </div>
-           
-         
-           
+                    
         );
     }
 }
